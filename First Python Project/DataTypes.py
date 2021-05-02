0ff1f7c60d56881a2993b0618e2a49a886b3cb22
JIO <javierx1993@gmail.com>
2021-05-02 12:38:24 -0700

    Adding Files and Notes

--- First Python Project/DataTypes.py
+++ First Python Project/DataTypes.py
@@ -1,2 +1,10 @@
 #The print command will show whatever is inside the parentheses in the terminal when the code is executed.
+
+#String Data Type
 print("200 is a great number")
+
+#Float Data Type
+print(3.5)
+
+#Integer Data Type
+print(3)
