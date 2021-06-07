4927abb50a6cb570ceb953cfd0ab3f9735003ee1
JIO93 <javierx1993@gmail.com>
2021-06-07 11:46:58 -0700

    adding another excecise

--- First Python Project/Functions.py
+++ First Python Project/Functions.py
@@ -1,10 +1,10 @@
-#Below you see us defining the variables. There are reserved words in Python that you can't use as a variable. You can look these up online. 
-#Make sure you use descriptive variable names. This way anyone who uses your code can easily see what your variables do.
 calculation_to_seconds = 24 * 60 * 60
 name_of_unit = "seconds"
 
-#Below you see how to insert the variables into the code. Python recognizes they are variables because they are inside of {}.
-print(f"20 days are {20 * calculation_to_seconds} {name_of_unit}")
-print(f"20 days are {35 * calculation_to_seconds} {name_of_unit}")
-print(f"20 days are {50 * calculation_to_seconds} {name_of_unit}")
-print(f"20 days are {110 * calculation_to_seconds} {name_of_unit}")
+#A function is definited by using keyword def. Place your code in the indented section below the function name.
+def days_to_units():
+    print(f"20 days are {20 * calculation_to_seconds} {name_of_unit}")
+    print("All Good!")
+
+#Functions are only run when they are called as shown below. If you don't call the fuction nothing is displayed.
+days_to_units()
